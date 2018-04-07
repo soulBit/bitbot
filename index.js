1d8d5e0b323ede22af5ae427608b5adddda62cf5
soulBit <its.soulBit@gmail.com>
2018-04-07 22:16:35 +0100

    first attempt at api call

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Discord.js bot
 const Discord = require('discord.js');
+const https = require('https');
+
 const client = new Discord.Client();
+const url = "https://www.bitstamp.net/api/ticker";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -8,7 +11,25 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '!ping') {
-    msg.reply('pong is updated');
+    msg.reply('pong!');
+  }
+  if (msg.content === '!price') {
+	https.get(url, res => {
+		res.setEncoding("utf8");
+		let body = "";
+		res.on("data", data => {
+			body += data;
+		});
+		res.on("end", () => {
+			body = JSON.parse(body);
+			// console.log(
+			// `City: ${body.results[0].formatted_address} -`,
+			// `Latitude: ${body.results[0].geometry.location.lat} -`,
+			// `Longitude: ${body.results[0].geometry.location.lng}`
+			// );
+			msg.reply(`${body.last}`);
+		});
+	});
   }
 });
 
