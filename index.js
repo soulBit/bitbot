e3ed2eb89865406a7da79ee1cb23b0657d4b2b66
soulBit <its.soulBit@gmail.com>
2018-04-07 22:32:14 +0100

    formatting

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const https = require('https');
 const client = new Discord.Client();
 const url = "https://www.bitstamp.net/api/v2/ticker/btcusd/";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+//https://www.bitstamp.net/api/
+//TODO: add currency pairs - btcusd, btceur, eurusd, xrpusd, xrpeur, xrpbtc, ltcusd, ltceur, ltcbtc, ethusd, etheur, ethbtc, bchusd, bcheur, bchbtc
+
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
 client.on('message', msg => {
   if (msg.content === '!ping') {
     msg.reply('pong!');
   }
-  if (msg.content === '!price') {
+  if (msg.content === '!btcprice') {
 	https.get(url, res => {
 		res.setEncoding("utf8");
 		let body = "";
@@ -22,7 +25,7 @@ client.on('message', msg => {
 		});
 		res.on("end", () => {
 			body = JSON.parse(body);
-			msg.reply(`\$${body.last}`);
+			msg.reply(`Latest Bitstamp price: \$${body.last}`);
 		});
 	});
   }
