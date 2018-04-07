a53496705bdc19220b1bcc4b09e29994b9bd7d07
Taylan Comert <tcomert@researchnow.com>
2018-04-07 16:28:48 +0100

    test

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
 
 client.login(process.env.TOKEN);
