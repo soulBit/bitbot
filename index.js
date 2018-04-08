30a688db5556b84ab75e35621ea1c1a99cead272
soulBit <its.soulBit@gmail.com>
2018-04-08 02:21:04 +0100

    adding currency pairs

--- index.js
+++ index.js
@@ -3,32 +3,56 @@ const Discord = require('discord.js');
 const https = require('https');
 
 const client = new Discord.Client();
-const url = "https://www.bitstamp.net/api/v2/ticker/btcusd/";
+const url = "https://www.bitstamp.net/api/v2/ticker/";
+const pairs = ["btcusd", "btceur", "eurusd", "xrpusd", "xrpeur", "xrpbtc", "ltcusd", "ltceur", "ltcbtc", "ethusd", "etheur", "ethbtc", "bchusd", "bcheur", "bchbtc"];
+const pairSymbol = ["$", "€", "$", "$", "€", "", "$", "€", "", "$", "€", "", "$", "€", "" ];
 
 //https://www.bitstamp.net/api/
-//TODO: add currency pairs - btcusd, btceur, eurusd, xrpusd, xrpeur, xrpbtc, ltcusd, ltceur, ltcbtc, ethusd, etheur, ethbtc, bchusd, bcheur, bchbtc
+//TODO: add currency pairs - 
 
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('pong!');
-  }
-  if (msg.content === '!btcprice') {
-	https.get(url, res => {
-		res.setEncoding("utf8");
-		let body = "";
-		res.on("data", data => {
-			body += data;
-		});
-		res.on("end", () => {
-			body = JSON.parse(body);
-			msg.reply(`Latest Bitstamp price: \$${body.last}`);
-		});
-	});
-  }
+    if (msg.content === '!ping') {
+        msg.reply('pong!');
+        return;
+    }
+
+    var content = msg.content.split(" ");
+    if (content[0] == "!price") {
+        var currPair = content[1];
+        if (typeof currPair !== "undefined" && currPair !== null)
+        {
+            var found = false;
+            for (var i = 0; i < pairs.length; i++)
+            {
+                if (currPair == pairs[i])
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                msg.reply("Currency pair '" + currPair + "' not found. Please try one of the following: " + pairs.join("','"));
+                return;
+            }
+
+            https.get(url + currPair, res => {
+            	res.setEncoding("utf8");
+            	let body = "";
+            	res.on("data", data => {
+            		body += data;
+            	});
+            	res.on("end", () => {
+            		body = JSON.parse(body);
+            		msg.reply("Latest Bitstamp price for " + currPair + ": " + pairSymbol[i] + `${body.last}`);
+            	});
+            });
+        }
+    }
 });
 
 client.login(process.env.TOKEN);
