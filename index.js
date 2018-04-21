c4ae14e159577fdad2bb37ab653a38ba982cb577
soulBit <its.soulBit@gmail.com>
2018-04-21 13:40:58 +0100

    fixing shit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Discord.js bot
 const Discord = require('discord.js');
 const https = require('https');
-//const request = require('request');
+const request = require('request');
 
 const client = new Discord.Client();
 const exchanges = {"bitstamp": {
@@ -11,43 +11,43 @@ const exchanges = {"bitstamp": {
                    "bitfinex": {
                     "symbolURL": "https://api.bitfinex.com/v1/symbols_details/",
                     "tickerURL": "https://www.bitstamp.net/api/v2/ticker/"
-                   }];
+                   }};
 var symbols = {};
 
-// function getBitstampData(){
-//     return new Promise(function(resolve, reject) {
-//         var req = https.get(exchanges.bitstamp.symbolURL, res => {
-//             // if (res.statusCode < 200 || res.statusCode >= 300) {
-//             //     return reject(new Error('statusCode=' + res.statusCode));
-//             // }
+function getBitstampData(){
+    return new Promise(function(resolve, reject) {
+        var req = https.get(exchanges.bitstamp.symbolURL, res => {
+            // if (res.statusCode < 200 || res.statusCode >= 300) {
+            //     return reject(new Error('statusCode=' + res.statusCode));
+            // }
 
-//             res.setEncoding("utf8");
-//             let body = "";
-//             res.on("data", data => {
-//                 body += data;
-//             });
-//             res.on("end", () => {
-//                 // body = JSON.parse(body);
-//                 // //msg.reply("Latest Bitstamp price for '" + currPair + "': " + pairSymbol[i] + `${body.last}`);
-//                 // symbols.bitstamp = [];
-//                 // body.forEach(function(symbol) {
-//                 //     symbols.bitstamp.push(symbol.url_symbol);
-//                 // });
-//                 try {
-//                     body = JSON.parse(body);
-//                 } catch(e) {
-//                     reject(e);
-//                 }
-//                 resolve(body);
-//             });
-//         });
-//          // reject on request error
-//         // req.on('error', function(err) {
-//         //     // This is not a "Second reject", just a different sort of failure
-//         //     reject(err);
-//         // });   
-//     });
-// }
+            res.setEncoding("utf8");
+            let body = "";
+            res.on("data", data => {
+                body += data;
+            });
+            res.on("end", () => {
+                // body = JSON.parse(body);
+                // //msg.reply("Latest Bitstamp price for '" + currPair + "': " + pairSymbol[i] + `${body.last}`);
+                // symbols.bitstamp = [];
+                // body.forEach(function(symbol) {
+                //     symbols.bitstamp.push(symbol.url_symbol);
+                // });
+                try {
+                    body = JSON.parse(body);
+                } catch(e) {
+                    reject(e);
+                }
+                resolve(body);
+            });
+        });
+         // reject on request error
+        // req.on('error', function(err) {
+        //     // This is not a "Second reject", just a different sort of failure
+        //     reject(err);
+        // });   
+    });
+}
 
 
 
@@ -63,63 +63,63 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-// client.on('message', msg => {
-//     if (msg.content === "!update")
-//     {
-//         getBitstampData().then(function(body){
-//             symbols.bitstamp = [];
-//             body.forEach(function(item) {
-//                 symbols.bitstamp.push(item.url_symbol);
-//             });
-//             msg.reply("Bitstamp markets updated: '" + symbols.bitstamp.join("','") + "'. ");
-//         });
-//     }
+client.on('message', msg => {
+    if (msg.content === "!update")
+    {
+        getBitstampData().then(function(body){
+            symbols.bitstamp = [];
+            body.forEach(function(item) {
+                symbols.bitstamp.push(item.url_symbol);
+            });
+            msg.reply("Bitstamp markets updated: '" + symbols.bitstamp.join("','") + "'. ");
+        });
+    }
 
-//     // if (msg.content === "!help")
-//     // {
-//     //     msg.reply("Use !price <market_name> with one of the following markets: '" + pairs.join("','") + "'. ");
-//     //     return;
-//     // }
+    // if (msg.content === "!help")
+    // {
+    //     msg.reply("Use !price <market_name> with one of the following markets: '" + pairs.join("','") + "'. ");
+    //     return;
+    // }
 
-//     // var content = msg.content.split(" ");
-//     // if (content[0] == "!price") {
-//     //     var currPair = content[1];
-//     //     if (typeof currPair !== "undefined" && currPair !== null)
-//     //     {
-//     //         var found = false;
-//     //         for (var i = 0; i < pairs.length; i++)
-//     //         {
-//     //             if (currPair == pairs[i])
-//     //             {
-//     //                 found = true;
-//     //                 break;
-//     //             }
-//     //         }
+    // var content = msg.content.split(" ");
+    // if (content[0] == "!price") {
+    //     var currPair = content[1];
+    //     if (typeof currPair !== "undefined" && currPair !== null)
+    //     {
+    //         var found = false;
+    //         for (var i = 0; i < pairs.length; i++)
+    //         {
+    //             if (currPair == pairs[i])
+    //             {
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
 
-//     //         if (!found)
-//     //         {
-//     //             msg.reply("Market '" + currPair + "' not found. Please try one of the following: '" + pairs.join("','") + "'.");
-//     //             return;
-//     //         }
+    //         if (!found)
+    //         {
+    //             msg.reply("Market '" + currPair + "' not found. Please try one of the following: '" + pairs.join("','") + "'.");
+    //             return;
+    //         }
 
-//     //         https.get(url + currPair, res => {
-//     //         	res.setEncoding("utf8");
-//     //         	let body = "";
-//     //         	res.on("data", data => {
-//     //         		body += data;
-//     //         	});
-//     //         	res.on("end", () => {
-//     //         		body = JSON.parse(body);
-//     //         		msg.reply("Latest Bitstamp price for '" + currPair + "': " + pairSymbol[i] + `${body.last}`);
-//     //         	});
-//     //         });
-//     //     }
-//     //     else
-//     //     {
-//     //         msg.reply("Use !price <market_name> with one of the following markets: '" + pairs.join("','") + "'. ");
-//     //         return;
-//     //     }
-//     // }
-// });
+    //         https.get(url + currPair, res => {
+    //         	res.setEncoding("utf8");
+    //         	let body = "";
+    //         	res.on("data", data => {
+    //         		body += data;
+    //         	});
+    //         	res.on("end", () => {
+    //         		body = JSON.parse(body);
+    //         		msg.reply("Latest Bitstamp price for '" + currPair + "': " + pairSymbol[i] + `${body.last}`);
+    //         	});
+    //         });
+    //     }
+    //     else
+    //     {
+    //         msg.reply("Use !price <market_name> with one of the following markets: '" + pairs.join("','") + "'. ");
+    //         return;
+    //     }
+    // }
+});
 
 client.login(process.env.TOKEN);
